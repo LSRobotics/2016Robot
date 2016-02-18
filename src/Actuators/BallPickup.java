50a90de569dc27c77c0964aabf38ca5ca3c9afc9
tylersiqi <tylersiqi@163.com>
2016-02-18 14:53:36 -0500

    Ball pickup with 1 button
    made ball pickup method with 1 button

--- src/Actuators/BallPickup.java
+++ src/Actuators/BallPickup.java
@@ -43,4 +43,39 @@ public class BallPickup {
 			}
 		}
 	}
+	
+	int state = 0; //0 as stop, 1 as take ball, 2 as shoot ball
+	
+	public void setBallIntake(double Mag) {
+		
+		if (Mag <= 0.2)state = 0;//if not triggered
+		else if (state == 0){
+			if (ballInTrap.get()){
+				
+				//if ball in trap
+				state = 2;
+			}
+			else state = 1; //if ball not in trap
+		}
+		
+		if (state == 0){//not triggered
+			left.set(0);
+			right.set(0);
+		}
+		if (state == 1){//take ball
+			if (ballInTrap.get()){
+				left.set(0.2);
+				right.set(-0.2);
+			}
+			else {
+				left.set(0);
+				right.set(0);
+			}
+		}
+		if (state == 2){//shoot ball
+			left.set(-1);
+		    right.set(1);
+		}
+		
+	}
 }
