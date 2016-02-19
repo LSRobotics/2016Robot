c52f581e3666f7868e4acff5d771d1449a96d9fb
Mark Bense <markbense2016@lschs.org>
2016-02-19 17:06:16 -0500

    SimpleClient.java added host name and port

--- src/org/usfirst/frc/team5181/robot/SimpleClient.java
+++ src/org/usfirst/frc/team5181/robot/SimpleClient.java
@@ -2,26 +2,32 @@ package org.usfirst.frc.team5181.robot;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 
 public class SimpleClient extends Thread {
 	
 	public volatile double centerX;
 	public volatile double centerY;
-	private int port;
+	private int port = 5805;
 	private Socket socket;
-	private SocketAddress ip;
+	private InetAddress ip;
 	
 	public SimpleClient() {
-		socket = new Socket();
+		try {
+			ip = InetAddress.getByName("192.168.1.174");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 		this.start();
 	}
 	
 	public void run() {
 		try {
-			socket.connect(ip, 12000);
+			socket = new Socket(ip, port);
 			DataInputStream dIn = new DataInputStream(socket.getInputStream());
 			while (true) {
 				byte[] packet = new byte[16];
