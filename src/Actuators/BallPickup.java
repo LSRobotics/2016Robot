c163dec7c7a18356246fbf348480f503cbb242fd
Alex Fang <frjalex@gmail.com>
2016-02-20 17:05:02 -0500

    ball pickup fixed

--- src/Actuators/BallPickup.java
+++ src/Actuators/BallPickup.java
@@ -9,52 +9,38 @@ import edu.wpi.first.wpilibj.Talon;
 
 public class BallPickup {
 	Talon left, right;
-	static double speedLimit = .3;
 	LimitSwitch ballInTrap;
-	boolean canLaunch;
 	
 	public BallPickup() {
 		left = new Talon(Statics.LeftBall);
 		right = new Talon(Statics.RightBall); 
 		
 		ballInTrap = new LimitSwitch(9);
-
-		canLaunch = false;
 	}
 	
-	int state = 0; //0 as stop, 1 as take ball, 2 as shoot ball
-	
-	public void setBallIntake(double Mag) {
-		
-		if (Mag <= 0.2)state = 0;//if not triggered
-		else if (state == 0){
-			if (!ballInTrap.get()){
-				
-				//if ball in trap
-				state = 2;
+	public void setBallIntake(double leftMag, double rightMag) {
+		if(!ballInTrap.get()) {
+			if(rightMag > .1) {
+				left.set(-1);
+				right.set(1);
+				return;
 			}
-			else state = 1; //if ball not in trap
+			else if(!(rightMag > .1)) {
+				left.set(0);
+				right.set(0);
+				return;
+			}
 		}
-		
-		if (state == 0){//not triggered
-			left.set(0);
-			right.set(0);
-		}
-		if (state == 1){//take ball
-			if (ballInTrap.get()){
+		if(!(rightMag > .1)) {
+			if(leftMag > 0.2) {
 				left.set(0.2);
 				right.set(-0.2);
 			}
-			else {
+			if(Math.abs(leftMag) < 0.1) {
 				left.set(0);
 				right.set(0);
 			}
 		}
-		if (state == 2){//shoot ball
-			left.set(-1);
-		    right.set(1);
-		}
-		
 	}
-	
 }
+
