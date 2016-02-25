bc8dbaa580e288c03baca8ed113eb7bfc13deb1d
Mark Bense <markbense2016@lschs.org>
2016-02-25 17:30:05 -0500

    Light.java: Periodic function now sets the color based on the alliance the robot is on, according to DriverStation.getInstance().getAlliance()

--- src/Sensors/Light.java
+++ src/Sensors/Light.java
@@ -2,11 +2,16 @@ package Actuators;
 
 import java.awt.Color;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
 public class Light{
 	Relay redRelay;
 	Relay greenRelay;
 	Relay blueRelay;
 	final Color GOLD = new Color(255, 215, 0);
+	final Color RED = new Color(255, 0, 0);
+	final Color BLUE = new Color(0, 0, 255);
 	
 	public Light(){
 		redRelay = new Relay(1,kForwardOnly);
@@ -24,9 +29,15 @@ public class Light{
 	
 	public void periodic(){
 		while (true){
-			//TODO if blue alliance, show blue
-			//TODO if red alliance, show red
-			this.setColor(GOLD);
+			if (DriverStation.getInstance().getAlliance().equals(Alliance.Blue)) {
+				this.setColor(BLUE);
+			}
+			else if (DriverStation.getInstance().getAlliance().equals(Alliance.Red)) {
+				this.setColor(RED);
+			}
+			else {
+				this.setColor(GOLD);
+			}
 		}
 	}
 }
