f76930ca05ceaf2811e10d0450ceb96d9e875758
Mark Bense <markbense2016@lschs.org>
2016-02-25 17:45:55 -0500

    Light.java: Added color enum, modified setColor() to take this enum as a parameter and set the blue, red, green relays to kOn or kOff based on the enum

--- src/Sensors/Light.java
+++ src/Sensors/Light.java
@@ -5,14 +5,22 @@ import java.awt.Color;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Relay.Value;
 
 public class Light{
 	Relay redRelay;
 	Relay greenRelay;
 	Relay blueRelay;
-	final Color GOLD = new Color(255, 215, 0);
-	final Color RED = new Color(255, 0, 0);
-	final Color BLUE = new Color(0, 0, 255);
+	//final Color GOLD = new Color(255, 215, 0);
+	//final Color RED = new Color(255, 0, 0);
+	//final Color BLUE = new Color(0, 0, 255);
+	enum Color {
+		RED,
+		GREEN,
+		BLUE,
+		OFF, //all off
+		ON   //all on
+	}
 	
 	public Light(){
 		redRelay = new Relay(1, Relay.Direction.kForward);
@@ -21,21 +29,50 @@ public class Light{
 	}
 	
 	public void setColor(Color c) {
-		redRelay.set( 1 - (c.getRed()/255));
-		greenRelay.set(1 - (c.getGreen()/255));
-		blueRelay.set(1 - (c.getBlue()/255));
+		switch (c) {
+			case RED:
+				redRelay.set(Value.kOn);
+				greenRelay.set(Value.kOff);
+				blueRelay.set(Value.kOff);
+				break;
+			case BLUE:
+				redRelay.set(Value.kOff);
+				greenRelay.set(Value.kOff);
+				blueRelay.set(Value.kOn);
+				break;
+			case GREEN:
+				redRelay.set(Value.kOff);
+				greenRelay.set(Value.kOn);
+				blueRelay.set(Value.kOff);
+				break;
+			case ON:
+				redRelay.set(Value.kOn);
+				greenRelay.set(Value.kOn);
+				blueRelay.set(Value.kOn);
+				break;
+			case OFF:
+				redRelay.set(Value.kOf);
+				greenRelay.set(Value.kOff);
+				blueRelay.set(Value.kOff);
+				break;
+			default:
+				break;
+		}
+		//redRelay.set( 1 - (c.getRed()/255));
+		//greenRelay.set(1 - (c.getGreen()/255));
+		//blueRelay.set(1 - (c.getBlue()/255));
 	}
 	
 	public void periodic(){
 		while (true){
 			if (DriverStation.getInstance().getAlliance().equals(Alliance.Blue)) {
-				this.setColor(BLUE);
+				this.setColor(Color.BLUE);
 			}
 			else if (DriverStation.getInstance().getAlliance().equals(Alliance.Red)) {
-				this.setColor(RED);
+				this.setColor(Color.RED);
 			}
 			else {
-				this.setColor(GOLD);
+				this.setColor(Color.ON);
 			}
 		}
 	}
