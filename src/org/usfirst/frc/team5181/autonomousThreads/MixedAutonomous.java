1f44fa3335ce0ee9988cf9ce2f96720eda3f1a3c
Alex Fang <frjalex@gmail.com>
2016-03-15 19:37:39 -0400

    Save Changes (to be continued)

--- src/org/usfirst/frc/team5181/autonomousThreads/MixedAutonomous.java
+++ src/org/usfirst/frc/team5181/autonomousThreads/MixedAutonomous.java
@@ -1,7 +1,37 @@
 package org.usfirst.frc.team5181.autonomousThreads;
 
-public class MixedAutonomous {
+import org.usfirst.frc.team5181.robot.DriveTrain;
+import org.usfirst.frc.team5181.robot.Gamepad;
+import org.usfirst.frc.team5181.robot.Robot;
+import org.usfirst.frc.team5181.sensors.SonicRangeSensor;
+import org.usfirst.frc.team5181.actuators.BallPickup;
 
-	private void ultrasonicRangeSensor () {}
+import edu.wpi.first.wpilibj.Timer;
+
+public class MixedAutonomous extends Thread {
+
+	SonicRangeSensor s = new SonicRangeSensor(0, 0); //TODO Map to port
+	boolean tStart = false;
+	Timer t = new Timer();
+	DriveTrain driveTrain;
+	private Robot robot;
+	BallPickup ballPickup = new BallPickup();
+	public void rangeSensingAuton() {
+		if(s.getRange(0) <= 11.811) {
+			if(s.getRange(0) <= 1.811) {
+				shoot();
+			} else {
+				cutThread();
+			}
+		} else {
+			robot.drive.arcadeDrive(0.0, -0.3);
+		}
+		
+	}
 	
+	public void cutThread() {}
+	public void shoot() {
+		ballPickup.setBallIntake( .2 , .2 );
+		
+	}
 }
--- src/org/usfirst/frc/team5181/sensors/SonicRangeSensor.java
+++ src/org/usfirst/frc/team5181/sensors/SonicRangeSensor.java
@@ -4,16 +4,19 @@ import edu.wpi.first.wpilibj.Ultrasonic;
 
 public class SonicRangeSensor {
 
-	static int ULTRASONIC_ECHO_PULSE_OUTPUT = 0;
-	static int ULTRASONIC_TRIGGER_PULSE_INPUT = 0;
+	static int ULTRASONIC_ECHO_PULSE_OUTPUT;
+	static int ULTRASONIC_TRIGGER_PULSE_INPUT;
 	Ultrasonic ultra = new Ultrasonic(ULTRASONIC_ECHO_PULSE_OUTPUT , ULTRASONIC_TRIGGER_PULSE_INPUT);
 
-	public SonicRangeSensor () {
+	public SonicRangeSensor (int pulseOutput, int pulseInput) {
+		ULTRASONIC_ECHO_PULSE_OUTPUT = pulseOutput;
+		ULTRASONIC_TRIGGER_PULSE_INPUT = pulseInput;
 		ultra.setAutomaticMode(true);
 	}
 
 	public double getRange (int i) {
-		if(i == 1)
+		//TODO add modes
+		if(i == 0)
 			return ultra.getRangeInches();
 		else
 			return ultra.getRangeMM();
