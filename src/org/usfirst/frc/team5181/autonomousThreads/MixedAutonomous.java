3c1d60d9333a962c49a05027528c6741e50d99f8
Alex Fang <frjalex@gmail.com>
2016-03-15 17:14:13 -0400

    New sensors class; New type of autonomous
    Added codes to prep for westtown

---
+++ src/org/usfirst/frc/team5181/autonomousThreads/MixedAutonomous.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team5181.autonomousThreads;
+
+public class MixedAutonomous {
+
+	private void ultrasonicRangeSensor () {}
+	
+}
---
+++ src/org/usfirst/frc/team5181/sensors/IRSensor.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team5181.sensors;
+
+public class IRSensor {
+
+}
