86e93c5b773654a3394d214bec18c8912676b41d
Alex Fang <frjalex@gmail.com>
2016-03-15 18:03:14 -0400

    Completed IR Sensor

--- src/org/usfirst/frc/team5181/sensors/IRSensor.java
+++ src/org/usfirst/frc/team5181/sensors/IRSensor.java
@@ -1,5 +1,22 @@
 package org.usfirst.frc.team5181.sensors;
 
-public class IRSensor {
+import edu.wpi.first.wpilibj.AnalogInput;
 
+public class IRSensor {
+	/**
+	 * @notice Might need tuning, see http://wpilib.screenstepslive.com/s/4485/m/13809/l/241876-analog-inputs
+	 */
+	AnalogInput ai = new AnalogInput(0);
+	
+	public double getDistance () {
+		/**
+		 * @params Returns the distence from Avg. voltage in double
+		 * Will be inaccurate in short distances
+		 * Some magic happens...
+		 * http://home.roboticlab.eu/en/examples/sensor/ir_distance
+		 */
+		return (5461 / (ai.getAverageVoltage() - 17) - 2);
+	}
+	
+	
 }
