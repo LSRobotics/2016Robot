cbb6b25d162046fa10e5c0428939d3054ce6367e
Alex Fang <frjalex@gmail.com>
2016-03-15 17:37:07 -0400

    Added code for Ultrasonic; Deleted interface RangeSensors

--- src/org/usfirst/frc/team5181/sensors/SonicRangeSensor.java
+++ src/org/usfirst/frc/team5181/sensors/SonicRangeSensor.java
@@ -1,5 +1,22 @@
 package org.usfirst.frc.team5181.sensors;
 
+import edu.wpi.first.wpilibj.Ultrasonic;
+
 public class SonicRangeSensor {
 
+	static int ULTRASONIC_ECHO_PULSE_OUTPUT = 0;
+	static int ULTRASONIC_TRIGGER_PULSE_INPUT = 0;
+	Ultrasonic ultra = new Ultrasonic(ULTRASONIC_ECHO_PULSE_OUTPUT , ULTRASONIC_TRIGGER_PULSE_INPUT);
+
+	public SonicRangeSensor () {
+		ultra.setAutomaticMode(true);
+	}
+
+	public double getRange (int i) {
+		if(i == 1)
+			return ultra.getRangeInches();
+		else
+			return ultra.getRangeMM();
+	}
+	
 }
